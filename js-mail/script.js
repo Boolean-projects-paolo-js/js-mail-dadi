3527157d2e3539dfb102cec6d268ac799d8aba0b
Paolo96700 <paolo.falcoapp@gmail.com>
2023-04-17 15:06:26 +0200

    Inizio esercizio

---
+++ js-mail/script.js
@@ -0,0 +1,29 @@
+
+//inserisci la tua mail
+const mail = prompt('inserisci la tua mail');
+
+//Iscritti
+const iscritti = ["luca.iscritto@gmail.com", "marco.iscritto@gmail.com", "paolo.iscritto@gmail.com"];
+
+for (let i = 0; i < iscritti.length; i++) {
+    //Stampa iscritti
+    console.log(iscritti[i]);
+
+    if (iscritti == true){
+        console.log (iscritti);
+    }
+
+    else if (iscritti == false){
+        console.log (errormail);
+        const errormail=alert("email non valida")
+            
+        }
+}
+
+
+
+
+
+
+
+
